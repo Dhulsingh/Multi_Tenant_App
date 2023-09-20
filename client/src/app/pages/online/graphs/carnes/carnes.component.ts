ab1c883773637a5b1f885342f7bbe8a2d093fbb7
dhulsingh <dhulsingh1@gmail.com>
2023-09-20 21:58:02 +0530

    new

--- client/src/app/pages/online/graphs/carnes/carnes.component.ts
+++ client/src/app/pages/online/graphs/carnes/carnes.component.ts
@@ -1,22 +1,33 @@
-import { DatePipe } from '@angular/common';
-import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { NbCalendarRange, NbColorHelper, NbThemeService } from '@nebular/theme';
-import { LocalDataSource, ViewCell } from 'ng2-smart-table';
-import { api } from '../../../../models/API';
-import { AnalyticsService } from '../../../../services/analytics.service';
-import { FacesService } from '../../../../services/faces.service';
-import JSMpeg from '@cycjimmy/jsmpeg-player';
-import { Router } from '@angular/router';
-import { Account } from '../../../../models/Account';
-import { WindowOpenerComponent } from '../window-opener/window-opener.component';
+import { DatePipe } from "@angular/common";
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  Renderer2,
+  ViewChild,
+} from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+import { NbCalendarRange, NbColorHelper, NbThemeService } from "@nebular/theme";
+import { LocalDataSource, ViewCell } from "ng2-smart-table";
+import { api } from "../../../../models/API";
+import { AnalyticsService } from "../../../../services/analytics.service";
+import { FacesService } from "../../../../services/faces.service";
+import { Router } from "@angular/router";
+import { Account } from "../../../../models/Account";
+import { NbDialogRef, NbDialogService } from "@nebular/theme";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { WindowOpenerComponent } from "../window-opener/window-opener.component";
+
 @Component({
   selector: 'ngx-carnes',
   templateUrl: './carnes.component.html',
   styleUrls: ['./carnes.component.scss']
 })
-export class CarnesComponent implements OnInit, OnDestroy {
-
+export class CarnesComponent implements OnInit , OnDestroy {
   @Input() range: NbCalendarRange<Date>;
   @Input() camera;
   carnes: any = [];
@@ -24,265 +35,251 @@ export class CarnesComponent implements OnInit, OnDestroy {
   timezone: any;
   now_user: Account;
   themeSubscription: any;
-  dataL: any;
-  optionsL: any;
-  options: any;
-  rtspIn: any;
-
-  @ViewChild('streaming', {static: false}) streamingcanvas: ElementRef;
+  options: any = {};
+  imageUrl: any;
+  dialogRef: NbDialogRef<any>;
+  @ViewChild("streaming", { static: false }) streamingcanvas: ElementRef;
+  canvas: any;
+  context: any;
+  data: any;
+  manualTriggerForm: FormGroup;
+  algorithms: any;
+  loading: boolean = false;
+  loadingTakeScreenShot: boolean = false;
+  algoId = 60;
 
   constructor(
     private serv: AnalyticsService,
     public sanitizer: DomSanitizer,
-    private theme: NbThemeService,
     private face: FacesService,
     public datepipe: DatePipe,
+    private theme: NbThemeService,
     private route: Router,
-  ) { }
+    private dialogService: NbDialogService,
+    private rd: Renderer2,
+    private fb: FormBuilder
+  ) {}
   single: any;
   colorScheme: any;
+  source: any = new LocalDataSource();
+  dataL: any;
+  optionsL: any;
+  count: number = 0;
+  coords = [];
 
-  ngOnDestroy(){
-    if(this.player !== undefined){
+  ngOnDestroy() {
+    if (this.player != undefined) {
       this.player.destroy();
-      this.face.cameraStop({id: this.camera}).subscribe(
-        res => {
-        },
-        err => console.error(err),
+      this.face.cameraStop({ id: this.camera }).subscribe(
+        (res) => {},
+        (err) => console.error(err)
       );
     }
   }
 
-  @ViewChild('videoPlayer', { static: false }) videoplayer: ElementRef;
+  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
   isPlay: boolean = false;
   toggleVideo(event: any) {
     this.videoplayer.nativeElement.play();
   }
-  videoFile: string = '';
-  pass(vid: string){
-    this.videoplayer.nativeElement.src = vid;
-    this.videoplayer.nativeElement.load();
-    this.videoplayer.nativeElement.play();
 
-  }
+  videoFile: string = "";
 
   video: boolean = false;
+  rtspIn: any;
 
   ngOnInit(): void {
-    this.now_user = JSON.parse(localStorage.getItem('now_user'));
-    const time = new Date();
-    this.timezone = time.toString().match(/[\+,\-](\d{4})\s/g)[0].split(' ')[0].slice(0, 3);
+    this.now_user = JSON.parse(localStorage.getItem("now_user"));
+    var time = new Date();
+    this.timezone = time
+      .toString()
+      .match(/[\+,\-](\d{4})\s/g)[0]
+      .split(" ")[0]
+      .slice(0, 3);
     this.timezone = parseInt(this.timezone);
-    let p = '';
-    if (this.timezone > 0){
-      p = '+';
+    let p = "";
+    if (this.timezone > 0) {
+      p = "+";
     }
-    this.timezone = p + JSON.stringify(this.timezone) + '00';
+    this.timezone = p + JSON.stringify(this.timezone) + "00";
     let type;
-    if (this.now_user.id_branch !== '0000'){
-      type = 'cam_id';
-    }else{
-      type = 'id_account';
+    if (this.now_user.id_branch != "0000") {
+      type = "cam_id";
+    } else {
+      type = "id_account";
     }
-    const l = {
+    let l = {
       start: this.range.start,
       end: this.range.end,
       type: type,
     };
-    this.face.checkVideo(17, this.camera).subscribe(
-      res => {
-        this.video = res['video'];
-        this.rtspIn = this.sanitizer.bypassSecurityTrustResourceUrl(res['http_out']);
-        if (this.video === true){
-          this.settings['columns']['picture'] = {
-            title: 'VIDEO',
-            type: 'custom',
-            filter: false,
-            renderComponent: ButtonViewComponent,
-            onComponentInitFunction: (instance) => {
-              instance.save.subscribe((row: string)  => {
-                this.pass(row);
-              });
-            },
-          };
-          this.settings = Object.assign({}, this.settings);
-        }
-      }, err => console.error(err),
+    // this.algoId = 19;
+    this.face.checkVideo(this.algoId, this.camera).subscribe(
+      (res) => {
+        this.video = res["video"];
+        this.link = res["http_out"];
+        this.rtspIn = this.sanitizer.bypassSecurityTrustResourceUrl(
+          res["http_out"]
+        );
+      },
+      (err) => console.error(err)
     );
-      this.serv.carnes(this.camera, l).subscribe(
-        res => {
-          this.carnes = res['data'];
-          for (const m of this.carnes.raw){
-            m['clip_path']  = api + '/pictures/' + this.now_user['id_account'] + '/' + m['id_branch'] + '/intrusion/' + m['cam_id'] + '/' + m['clip_path'];
-            m['picture']  = this.sanitizer.bypassSecurityTrustUrl(api + '/pictures/' + this.now_user['id_account'] + '/' + m['id_branch'] + '/intrusion/' + m['cam_id'] + '/' + m['picture']);
-            m['time'] = this.datepipe.transform(m['time'], 'yyyy-M-dd HH:mm:ss', '+0000');
-            m['videoClip']  = this.sanitizer.bypassSecurityTrustUrl(api + '/pictures/' + this.now_user['id_account'] + '/' + m['id_branch'] + '/intrusion/' + m['cam_id'] + '/' + m['movie']);
+    this.serv.carnes(this.camera, l).subscribe(
+      (res) => {
+        this.carnes = res["data"];
+        for (var m of this.carnes.raw) {
+          m["picture"] = this.sanitizer.bypassSecurityTrustUrl(
+            api +
+              "/pictures/" +
+              this.now_user["id_account"] +
+              "/" +
+              m["id_branch"] +
+              "/carnes/" +
+              m["cam_id"] +
+              "/" +
+              m["picture"]
+          );
+          m["clip_path"] =
+            api +
+            "/pictures/" +
+            this.now_user["id_account"] +
+            "/" +
+            m["id_branch"] +
+            "/carnes/" +
+            m["cam_id"] +
+            "/" +
+            m["clip_path"];
+            m['videoClip']  = this.sanitizer.bypassSecurityTrustUrl(api + '/pictures/' + this.now_user['id_account'] + '/' + m['id_branch'] + '/carnes/' + m['cam_id'] + '/' + m['movie']);
+          m["time"] = this.datepipe.transform(m["time"], "yyyy-M-dd HH:mm:ss");
+          switch (m["severity"]) {
+            case "0": {
+              m["severity"] = "Low";
+              break;
+            }
+            case "1": {
+              m["severity"] = "Mid";
+              break;
+            }
+            case "2": {
+              m["severity"] = "High";
+              break;
+            }
           }
-          this.source = this.carnes.raw.slice().sort((a, b) => +new Date(b.time) - +new Date(a.time));
-          if (this.carnes.donut.length !== 0){
-            this.source2 = this.carnes.donut;
+        }
+        let labels = [];
+        for (var o of Object.keys(this.carnes.over)) {
+          o = o + ":00:00";
+          labels.push(this.datepipe.transform(o, "yyyy-M-dd HH:mm"));
+        }
+        this.source = this.carnes.raw.slice().sort((a, b) => +new Date(b.time) + +new Date(a.time));
+        this.themeSubscription = this.theme.getJsTheme().subscribe((config) => {
+          const colors: any = config.variables;
+          const chartjs: any = config.variables.chartjs;
 
-          const labels = [];
-          for (let o of Object.keys(this.carnes.over)){
-            o = o + ':00:00';
-            labels.push(this.datepipe.transform(o, 'yyyy-M-dd HH:mm', '+0000'));
-          }
-
-            this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
-
-
-            const colors: any = config.variables;
-            const chartjs: any = config.variables.chartjs;
-
-            this.dataL = {
-              labels: labels,
-              datasets: [{
-                label: 'Intrusion Over Time',
+          this.dataL = {
+            labels: labels,
+            datasets: [
+              {
+                label: "Hands Over Time",
                 backgroundColor: NbColorHelper.hexToRgbA(colors.primary, 0.3),
                 data: Object.values(this.carnes.over),
                 borderColor: colors.primary,
-              }],
-            };
+              },
+            ],
+          };
 
-            this.optionsL = {
-              responsive: true,
-              maintainAspectRatio: false,
-              legend: {
-                display: false,
-                position: 'bottom',
-                labels: {
-                  fontColor: chartjs.textColor,
-                },
+          this.optionsL = {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+              display: false,
+              position: "bottom",
+              labels: {
+                fontColor: chartjs.textColor,
               },
-              hover: {
-                mode: 'index',
-              },
-              scales: {
-                xAxes: [
-                  {
+            },
+            hover: {
+              mode: "index",
+            },
+            scales: {
+              xAxes: [
+                {
+                  display: false,
+                  scaleLabel: {
                     display: false,
-                    scaleLabel: {
-                      display: false,
-                      labelString: 'Month',
-                    },
-                    gridLines: {
-                      display: true,
-                      color: chartjs.axisLineColor,
-                    },
-                    ticks: {
-                      fontColor: chartjs.textColor,
-                    },
+                    labelString: "Month",
                   },
-                ],
-                yAxes: [
-                  {
+                  gridLines: {
                     display: true,
-                    scaleLabel: {
-                      display: false,
-                      labelString: 'Value',
-                    },
-                    gridLines: {
-                      display: true,
-                      color: chartjs.axisLineColor,
-                    },
-                    ticks: {
-                      fontColor: chartjs.textColor,
-                    },
+                    color: chartjs.axisLineColor,
                   },
-                ],
-              },
-            };
-
-              this.options = {
-                // backgroundColor: echarts.bg,
-                color: [colors.warningLight, colors.infoLight, colors.dangerLight, colors.successLight, colors.primaryLight],
-                tooltip: {
-                  trigger: 'item',
-                  formatter: '{a} <br/>{b} : {c} ({d}%)',
-                },
-                legend: {
-                  orient: 'vertical',
-                  left: 'left',
-                  data: Object.keys(this.carnes.donut),
-                  textStyle: {
-                    // color: echarts.textColor,
+                  ticks: {
+                    fontColor: chartjs.textColor,
                   },
                 },
-                series: [
-                  {
-                    name: 'Zones',
-                    type: 'pie',
-                    radius: '80%',
-                    center: ['50%', '50%'],
-                    data: this.carnes.donut,
-                    itemStyle: {
-                      emphasis: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        // shadowColor: echarts.itemHoverShadowColor,
-                      },
-                    },
-                    label: {
-                      normal: {
-                        textStyle: {
-                          // color: echarts.textColor,
-                        },
-                      },
-                    },
-                    labelLine: {
-                      normal: {
-                        lineStyle: {
-                          // color: echarts.axisLineColor,
-                        },
-                      },
-                    },
+              ],
+              yAxes: [
+                {
+                  display: true,
+                  scaleLabel: {
+                    display: false,
+                    labelString: "Value",
                   },
-                ],
-              };
-            });
-        }
-        },
-        err => console.error(err),
-      );
+                  gridLines: {
+                    display: true,
+                    color: chartjs.axisLineColor,
+                  },
+                  ticks: {
+                    fontColor: chartjs.textColor,
+                  },
+                },
+              ],
+            },
+          };
+        });
+      },
+      (err) => console.error(err)
+    );
   }
 
-  source: any = new LocalDataSource();
-  source2: any = new LocalDataSource();
-  got(id){
-    this.route.navigate([`/pages/tickets`]);
-    // this.route.navigate([`/pages/tickets/view/${id.data.id}`], {queryParams: id.data})
-}
-
-  settings = {
-    mode: 'external',
-    actions: {
-      position: 'right',
-      columnTitle: 'ACTIONS',
-      add: false,
-      edit: true,
-      delete: false,
-    },
-    edit: {
-      editButtonContent: '<i class="fas fa-ellipsis-h"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-      confirmSave: true,
-    },
-    pager : {
-      display : true,
-      perPage: 5,
+  getAlgorithms() {
+    this.face.getAllAlgos().subscribe(
+      (res: any) => {
+        this.algorithms = res.data;
       },
-    noDataMessage: 'No data found',
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  link: string;
+
+  got(id) {
+    this.route.navigate([`/pages/tickets`]);
+  }
+
+  pass(vid: string) {
+    this.videoplayer.nativeElement.src = vid;
+    this.videoplayer.nativeElement.load();
+    this.videoplayer.nativeElement.play();
+  }
+  settings = {
+    mode: "external",
+    actions: false,
+    pager: {
+      display: true,
+      perPage: 5,
+    },
+    noDataMessage: "No data found",
     columns: {
       picture: {
-        title: 'PICTURE',
-        type: 'custom',
+        title: "PICTURE",
+        type: "custom",
         filter: false,
         renderComponent: ButtonViewComponentPic,
         onComponentInitFunction: (instance) => {
-          instance.save.subscribe((row: string)  => {
-            // this.pass(row)
-          });
+          instance.save.subscribe((row: string) => {});
         },
       },
       movie: {
@@ -297,107 +294,29 @@ export class CarnesComponent implements OnInit, OnDestroy {
         },
       },
       time: {
-        title: 'TIME',
-        type: 'string',
+        title: "TIME",
+        type: "string",
         filter: false,
       },
       camera_name: {
-        title: 'CAM',
-        type: 'string',
-        filter: false,
-      },
-      zone: {
-        title: 'ZONE',
-        type: 'string',
+        title: "CAM",
+        type: "string",
         filter: false,
       },
     },
   };
-
-  settings2 = {
-    mode: 'external',
-    actions: {
-      position: 'right',
-      add: false,
-      edit: false,
-      delete: false,
-    },
-    pager : {
-      display : true,
-      perPage: 5,
-      },
-    noDataMessage: 'No data found',
-    columns: {
-      name: {
-        title: 'ZONE',
-        type: 'string',
-        filter: false,
-      },
-      value: {
-        title: 'TOTAL',
-        type: 'string',
-        filter: false,
-      },
-      perc: {
-        title: 'PERCENTAGE',
-        type: 'string',
-        filter: false,
-      },
-    },
-  };
-
-}
-
-
-
-@Component({
-  selector: 'button-view',
-  styles: ['.play-btn { position: absolute; left: 50%; top: 50%; margin-top: -17px; margin-left: -20px; color: #f7f9fc47}'],
-  template: `
-  <div >
-  <div style = "width:60px; height: 60px">
-    <img [src]="rowData.picture" width='60' height='60'>
-    <button class='btn btn-link play-btn' (click)="openVideo()"><i class="fas fa-play"></i></button>
-  </div>
-</div>
-  `,
-})
-export class ButtonViewComponent implements ViewCell, OnInit {
-  renderValue: string;
-
-  constructor(){
-  }
-
-  @Input() value: string | number;
-  @Input() rowData: any;
-
-  @Output() save: EventEmitter<any> = new EventEmitter();
-
-  openVideo(){
-    this.save.emit(this.rowData.clip_path);
-  }
-
-  ngOnInit() {
-    this.renderValue = this.value.toString().toUpperCase();
-  }
 }
 
 @Component({
-  selector: 'button-view',
-  template: `
-    <img [src]="rowData.picture" width='60' height='60'>
-  `,
+  selector: "button-view",
+  template: ` <img [src]="rowData.picture" width="60" height="60" /> `,
 })
 export class ButtonViewComponentPic implements ViewCell, OnInit {
-
-  constructor(){
-  }
+  constructor() {}
 
   @Input() value: string | number;
   @Input() rowData: any;
   @Output() save: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
-
